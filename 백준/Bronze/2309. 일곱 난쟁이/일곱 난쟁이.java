348d7134dd7ed53e33f26c11f20b65c20a2b41be
SeonggeunPark <162011004+SeonggeunPark@users.noreply.github.com>
2024-08-04 23:11:31 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 100 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] arr = new int[9];
+		int sum = 0;
+		for (int i=0; i<9; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			sum += arr[i];
+		}
+		
+		out : for (int i=0; i<8; i++) {
+			for (int j=i+1; j<9; j++) {
+				if (sum-arr[i]-arr[j] == 100) {
+					arr[i] = -1;
+					arr[j] = -1;
+					break out;
+				}
+			}
+		}
+		
+		Arrays.sort(arr);
+		for (int i=2; i<9; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+}
