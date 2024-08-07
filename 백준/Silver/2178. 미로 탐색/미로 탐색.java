71c5cf221ce74786bfe5e520fa1fb18d401ca467
SeonggeunPark <162011004+SeonggeunPark@users.noreply.github.com>
2024-08-07 23:40:16 +0900

    [Silver I] Title: 미로 탐색, Time: 128 ms, Memory: 15908 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -2,102 +2,77 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static boolean[][] visited;
-	static int[][] map;
-	static int M, N;
-	static int count;
+    static boolean[][] visited;
+    static int[][] map;
+    static int M, N;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        
+        // map 입력
+        String str;
+        for (int i=0; i<N; i++) {
+            str = br.readLine();
+            for(int j=0; j<M; j++) {
+                map[i][j] = Character.getNumericValue(str.charAt(j));
+            }
+        }
+        
+        visited = new boolean[N][M];
+        
+        BFS();
+        
+        System.out.println(map[N-1][M-1]);
+        
+    }
+        
+    static int[] dr = { -1, 1, 0, 0 };
+    static int[] dc = { 0, 0, -1, 1 };
+    static int nr, nc;
+    static ArrayList<Integer> pos, npos; // 큐에 담을 좌표값 선언
+    static int count = 0;
 
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		map = new int[N][M];
+    static void BFS() {
+        Queue<ArrayList> queue = new LinkedList<>();
 
-		// map 입력
-		String str;
-		for (int i = 0; i < N; i++) {
-			str = br.readLine();
-			for (int j = 0; j < M; j++) {
-				map[i][j] = Character.getNumericValue(str.charAt(j));
-			}
-		}
+        if (visited[0][0])
+            return;
 
-		visited = new boolean[N][M];
+        pos = new ArrayList<>();
+        pos.add(0);
+        pos.add(0);
 
-		BFS();
+        queue.offer(pos);
+        visited[0][0] = true;
+        while (visited[visited.length - 1][visited[0].length - 1] == false) {
+            pos = queue.poll(); // (1)
 
-        System.out.println(count);
+            for (int idx = 0; idx < 4; idx++) {
+                npos = new ArrayList<>();
 
-	}
+                nr = pos.get(0) + dr[idx];
+                nc = pos.get(1) + dc[idx];
 
-	// 상 하 좌 우
-	static int[] dr = { -1, 1, 0, 0 };
-	static int[] dc = { 0, 0, -1, 1 };
-	static int nr, nc;
-	static ArrayList<Integer> pos, npos; // 큐에 담을 좌표값 선언
-	static ArrayList<ArrayList<Integer>> poses, nposes; // 큐에 담을 좌표 모음
-	// BFS 메서드
+                if (0 <= nr && nr < map.length && 0 <= nc && nc < map[0].length && map[nr][nc] == 1
+						&& visited[nr][nc] == false) {
 
-	static void BFS() {
-		Queue<ArrayList<ArrayList<Integer>>> queue = new LinkedList<>();
-		// 이미 방문했으면 종료
-		if (visited[0][0])
-			return;
-		// 좌표값 저장할 리스트 변수 선언 및 현재 위치(0,0) 지정
-		pos = new ArrayList<>();
-		poses = new ArrayList<>();
-		pos.add(0);
-		pos.add(0);
-
-		poses.add(pos);
-
-		// 현재 위치 방문 처리 & 큐에 삽입
-		queue.offer(poses);
-		visited[0][0] = true;
-		count++;
-
-		// 도착지점 방문 체크될 때까지 반복
-		while (visited[visited.length - 1][visited[0].length - 1] == false) {
-			// (1)큐에 노드 위치 제거 =>
-			// (2)인접 노드 큐에 삽입 =>
-			// (3)방문 처리
-			// 반복
-			poses = queue.poll(); // (1)
-//            System.out.println(pos);
-//			int searchCount = 0;
-			nposes = new ArrayList<>();
-			for (ArrayList<Integer> pos : poses) {
-				for (int idx = 0; idx < 4; idx++) {
-					// npos 계속 초기화해야 리스트 사이즈 2개 유지됨
-					npos = new ArrayList<>();
-//                System.out.println(queue);
-					// 상하좌우가 범위 넘어가지 않으면서,
-					// 배추가 심어져 있으면서,
-					// 방문한 적 없으면
-					// => 큐에 삽입 + 방문 처리
-					nr = pos.get(0) + dr[idx];
-					nc = pos.get(1) + dc[idx];
-					if (0 <= nr && nr < map.length && 0 <= nc && nc < map[0].length && map[nr][nc] == 1
-							&& visited[nr][nc] == false) {
-
-						npos.add(nr);
-						npos.add(nc);
-						nposes.add(npos); // (2)
-						visited[nr][nc] = true; // (3)
-//						searchCount++;
-					}
-				}
-			}
-			queue.add(nposes);
-			count++;
-		}
-	}
+                    npos.add(nr);
+                    npos.add(nc);
+                    queue.offer(npos); // (2)
+                    visited[nr][nc] = true; // (3)
+                    map[nr][nc] = map[pos.get(0)][pos.get(1)]+1;
+                }
+            }
+        }
+    }
 }
