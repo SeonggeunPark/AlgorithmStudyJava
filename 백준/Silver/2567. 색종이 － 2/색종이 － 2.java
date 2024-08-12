c11dba5183011e0827955679ca528d3bec4645e1
SeonggeunPark <162011004+SeonggeunPark@users.noreply.github.com>
2024-08-13 00:54:18 +0900

    [Silver IV] Title: 색종이 - 2, Time: 176 ms, Memory: 17800 KB -BaekjoonHub

--- 백준/Silver/2567. 색종이 － 2/색종이 － 2.java
+++ 백준/Silver/2567. 색종이 － 2/색종이 － 2.java
@@ -1,7 +1,13 @@
 import java.util.Scanner;
-
+/*
+ * 처음엔 연산을 줄이기 위해 종이가 붙었던 좌표를 저장하여
+ * 해당 구간을 탐색하면 효율적인 코드가 될 것이라 생각했으나,
+ * 그렇게 처리하면 테두리가 겹칠 때 중복으로 처리가 되는 것을 뒤늦게 발견.
+ * => 100*100 배열을 모두 1번씩 돌며 true일 때만 탐색하는 방식이
+ * 	  정확한 답을 출력할 수 있음
+ */
 public class Main {
-
+	
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
@@ -11,11 +17,15 @@ public class Main {
         int nr, nc;
         int cnt = 0;
         boolean[][] papers = new boolean[100][100];
+        int[][] pos = new int[N][2];
         
-        // 색종이 붙이기 : 좌표부터 행 열 각각 +10만큼 true로 변환
+        // 색종이 붙이기 : 좌표부터 행 열 각각 +10만큼 true로 변환 &&
+        // 색종이 붙은 부분만 탐색하기 위해 pos 배열에 담아둠 => 잘못된 방법
         for (int t=0; t<N; t++) {
             int r = sc.nextInt();
             int c = sc.nextInt();
+//            pos[t][0] = r;
+//            pos[t][1] = c;
             for (int i = r; i < r+10; i++) {
                 for (int j = c; j < c+10; j++) {
                     papers[i][j] = true;
@@ -46,5 +56,32 @@ public class Main {
         }
         
         System.out.println(cnt);
+//        for (int t=0; t<N; t++) {
+//            int y = pos[t][0];
+//            int x = pos[t][1];
+//            
+//            for (int r = y; r < y+10; r++) {
+//                for (int c = x; c < x+10; c++) {
+//                	
+//                	// 상하좌우 탐색하여 빈공간 카운트
+//                    for (int idx=0; idx<4; idx++) {
+//                    	nr = r+dr[idx];
+//                    	nc = c+dc[idx];
+//                    	
+//                    	// 배열 범위 이내이면 탐색 후 빈공간일 때 카운트 +1
+//                    	if (0<=nr && nr<100 && 0<=nc && nc<100) {
+//                    		if (papers[nr][nc] == false) {
+//                        		cnt++;
+//                        	}
+//                    		// 범위 밖이면 무조건 빈공간이므로 카운트 +1
+//                    	} else cnt++;     	
+//                    }
+//                	// 첫행과 마지막 행 제외하고, 행의 양 끝 열만 탐색 (사각형 테두리만 탐색)
+//                	if (r!=y && r!= y+9 && c==x) c+=8;
+//                }
+//            }
+//        }
+//        
+//        System.out.println(cnt);
     }
 }
