4478d0cc1797db66f359095dc9bd953c5b701f14
SeonggeunPark <162011004+SeonggeunPark@users.noreply.github.com>
2024-08-14 09:43:30 +0900

    [Bronze I] Title: 비밀 이메일, Time: 120 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Bronze/2999. 비밀 이메일/비밀 이메일.java
+++ 백준/Bronze/2999. 비밀 이메일/비밀 이메일.java
@@ -2,6 +2,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/* 2차원 배열 생성 없이 바로 StringBuilder로 뽑아내는 방법으로
+ * 나름 효율적이었다고 생각했지만, 배열은 애초에 빨라서 실행시간과 크게 연관이 없는 것으로 보임
+ * 처음 값을 입력받을 때부터 열과 행을 뒤집어 받은 후 그대로 출력하면
+ * 덜 헷갈리고 더 쉽게 풀수 있었음. 
+ */
+
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,8 +27,6 @@ public class Main {
 			}
 		}
 
-		char[][] message = new char[r][c];
-		
 		// 0행부터 순서대로 뽑기 위해
 		// 0부터 r개씩 건너뛰면서 문자 추출.
 		// 1부터 r개씩 건너뛰며 추출 
