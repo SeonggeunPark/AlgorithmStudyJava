98f768370fc09d1f24a11c6e020cbb2378944b1f
SeonggeunPark <162011004+SeonggeunPark@users.noreply.github.com>
2024-08-15 01:00:10 +0900

    [Bronze II] Title: 나머지, Time: 168 ms, Memory: 17576 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,26 +1,16 @@
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-        // 배열을 -1로 초기화 하지 않으면 값이 0으로 자동 초기화됨.
-        // 그렇게 될 경우 나머지가 0인 상황에서 중복되는 값으로 인식되어
-        // 카운트되지 않는 오류가 발생.
-		int[] arr = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
-		int cnt = 0;
+		Set<Integer> set = new HashSet<>();
 		
 		out: for (int i=0; i<10; i++) {
-			int n = sc.nextInt() % 42;
-			
-			for (int j=0; j<=i-1; j++) {
-				if (arr[j] == n) {
-					continue out;
-				}
-			}
-			arr[i] = n;
-			cnt++;
+			set.add(sc.nextInt() % 42);
 		}
 		
-		System.out.println(cnt);
+		System.out.println(set.size());
 	}
 }
