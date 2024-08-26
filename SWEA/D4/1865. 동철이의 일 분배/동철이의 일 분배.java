649a24635679ccc91b763710c0afe9ed74818f94
SeonggeunPark <162011004+SeonggeunPark@users.noreply.github.com>
2024-08-26 22:40:58 +0900

    [D4] Title: 동철이의 일 분배, Time: 2,223 ms, Memory: 23,972 KB -BaekjoonHub

--- SWEA/D4/1865. 동철이의 일 분배/동철이의 일 분배.java
+++ SWEA/D4/1865. 동철이의 일 분배/동철이의 일 분배.java
@@ -8,6 +8,10 @@ import java.util.StringTokenizer;
  * N명이 N개의 일을 각각 성공할 확률이 모두 다르므로 N*N 배열 생성
  * 1부터 N까지 나열하는 가능한 모든 순열 조합을 구한 후 해당하는 확률을 곱하여
  * 최종 확률을 구함. 그 중 최대값을 찾아 출력
+ * 
+ * 1. 순열 조합으로 작서앟였으나 실행시간 초과되어 순열 배열을 생성하는 과정을 생략한 재귀함수를 사용
+ * 2. 재귀함수도 실행시간 초과되었으나, 재귀 과정에서 max의 확률보다 같거나 작으면 더이상 연산이 필요 없음을
+ * 	  발견하여 해당 부분을 가지치기하여 해결함
  */
 public class Solution {
 	static int[][] workChance;
@@ -24,11 +28,7 @@ public class Solution {
 
 		for (int t = 1; t <= T; t++) {
 			N = Integer.parseInt(br.readLine());
-			// 순열 만들 0부터 N-1까지 담긴 배열 생성
-			order = new int[N];
-			for (int i = 0; i < N; i++) {
-				order[i] = i;
-			}
+			// 중복된 경우의 수를 막기 위한 방문 배열 생성
 			visited = new boolean[N];
 			// N*N 배열 생성하여 각각 확률 입력
 			workChance = new int[N][N];
@@ -54,7 +54,6 @@ public class Solution {
 		if (chance<=max) return;
 		// 마지막 직원까지 모두 일 맡으면 최종 확률 계산
 		if (k >= N) {
-//			System.out.println(max);
 			max = chance;
 			return;
 		}
@@ -64,10 +63,8 @@ public class Solution {
 		// i번 직원이 1번 일 할 때, 2번 일 할 떄 .... N번 일 할 때
 		for (int i = 0; i < N; i++) {
 			if (!visited[i]) {
-//				System.out.println("계산전 chance = "+chance);
 				chance *= (double)workChance[k][i] / 100;
 				visited[i] = true;
-//				System.out.println("k="+k+" i="+i+" chance="+chance);
 				makeOrder(k+1, chance);
 				chance = beforeChance;
 				visited[i] = false;
