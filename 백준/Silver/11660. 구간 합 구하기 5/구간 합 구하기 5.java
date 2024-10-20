7f149c3350f4edd2b1d803d99ae8e36c0ab7f110
SeonggeunPark <162011004+SeonggeunPark@users.noreply.github.com>
2024-10-21 00:26:39 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1532 ms, Memory: 319200 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,38 +1,58 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        // 입력받을 N, M
-        int N = scanner.nextInt();
-        int M = scanner.nextInt();
-        
-        // 2차원 배열과 누적합 배열 초기화
-        int[][] arr = new int[N + 1][N + 1];
-        int[][] sum = new int[N + 1][N + 1];
-
-        // 2차원 배열 입력 받기
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                arr[i][j] = scanner.nextInt();
-                // 누적합 계산
-                sum[i][j] = arr[i][j] + sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1];
-            }
-        }
-
-        // 쿼리 처리
-        for (int k = 0; k < M; k++) {
-            int x1 = scanner.nextInt();
-            int y1 = scanner.nextInt();
-            int x2 = scanner.nextInt();
-            int y2 = scanner.nextInt();
-            
-            // 구간합 계산
-            int result = sum[x2][y2] - sum[x1 - 1][y2] - sum[x2][y1 - 1] + sum[x1 - 1][y1 - 1];
-            System.out.println(result);
-        }
-        
-        scanner.close();
-    }
+	static class Zone {
+		int r1, c1, r2, c2;
+		public Zone() {
+		}
+		@Override
+		public String toString() {
+			return "Zone [r1=" + r1 + ", c1=" + c1 + ", r2=" + r2 + ", c2=" + c2 + "]";
+		}
+		public Zone(int r1, int c1, int r2, int c2) {
+			super();
+			this.r1 = r1;
+			this.c1 = c1;
+			this.r2 = r2;
+			this.c2 = c2;
+		}
+	}
+	static StringBuilder sb = new StringBuilder();
+	static int N, M;
+	static int max;
+	static Zone[] zones;
+	static int[][] matrix;
+	static int[][] sum;
+	public static void main(String[] args) {
+		init(); // 입력 및 초기화
+		
+		for (Zone zone : zones) 
+			sb.append(cal(zone)).append('\n');
+		
+		System.out.println(sb);
+	}
+	private static void init() {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		
+		// 행렬 입력
+		matrix = new int[N+1][N+1];
+		sum = new int[N+1][N+1];
+		for (int r=1; r<=N; r++) {
+			for (int c=1; c<=N; c++) {
+				matrix[r][c] = sc.nextInt();
+				sum[r][c] = matrix[r][c] + sum[r-1][c] + sum[r][c-1] - sum[r-1][c-1];
+			}
+		}
+		
+		// M개만큼의 구해야할 구역을 객체로 생성해 배열로 관리
+		zones = new Zone[M];
+		for (int i=0; i<M; i++) {
+			zones[i] = new Zone(sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt());
+		}
+	}
+	static int cal(Zone zone) {
+		return sum[zone.r2][zone.c2] - sum[zone.r1-1][zone.c2]- sum[zone.r2][zone.c1-1] + sum[zone.r1-1][zone.c1-1];
+	}
 }
