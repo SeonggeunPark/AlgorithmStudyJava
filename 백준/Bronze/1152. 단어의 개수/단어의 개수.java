f3c572637a2eb7237dfd25755013d5cd8aa09dd2
SeonggeunPark <162011004+SeonggeunPark@users.noreply.github.com>
2024-11-20 21:01:42 +0900

    [Bronze II] Title: 단어의 개수, Time: 324 ms, Memory: 43504 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int cnt = 0;
+		while(st.hasMoreTokens()) {
+			String str = st.nextToken();
+			cnt += 1;
+		}
+		
+		System.out.println(cnt);
+	}
+}
