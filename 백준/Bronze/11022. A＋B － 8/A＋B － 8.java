56f83d81d130a33771ebe01ba52265820cba98a0
SeonggeunPark <162011004+SeonggeunPark@users.noreply.github.com>
2024-11-22 23:20:22 +0900

    [Bronze V] Title: A+B - 8, Time: 220 ms, Memory: 19052 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,25 @@
+ import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for (int i = 1; i <= T; i++) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			
+			System.out.println("Case #"+i+": "+A+" + "+B+" = "+(A+B));
+		}
+	}
+}
