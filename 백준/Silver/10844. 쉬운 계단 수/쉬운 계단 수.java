51f18b51c4fef594c34ad16d2d0fa54e1e58a1f5
SeonggeunPark <162011004+SeonggeunPark@users.noreply.github.com>
2024-12-18 00:14:15 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 108 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -1,36 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.Scanner;
+import java.util.StringTokenizer;
+/*
+ */
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        // 입력: N 값
-        int N = sc.nextInt();
-        
-        // DP 배열 선언: dp[i][j]는 i자리 수에서 끝자리가 j인 계단 수의 개수
-        long[][] dp = new long[N + 1][10];
-        
-        // 초기 상태 설정 (길이가 1인 계단 수는 1부터 9까지 각각 1개씩)
-        for (int j = 1; j <= 9; j++) {
-            dp[1][j] = 1;
-        }
-        
-        // DP 배열 채우기
-        for (int i = 2; i <= N; i++) {
-            for (int j = 0; j <= 9; j++) {
-                if (j > 0) dp[i][j] += dp[i - 1][j - 1];  // 이전 자리가 j-1
-                if (j < 9) dp[i][j] += dp[i - 1][j + 1];  // 이전 자리가 j+1
-                dp[i][j] %= 1000000000;  // 1,000,000,000으로 나눈 나머지
-            }
-        }
-        
-        // 결과 계산 (N자리 수의 계단 수의 합)
-        long result = 0;
-        for (int j = 0; j <= 9; j++) {
-            result += dp[N][j];
-        }
-        
-        // 결과 출력
-        System.out.println(result % 1000000000);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		
+		// N자리 계단수 중 끝자리가 0~9인 수의 개수를 각각 저장하는 배열
+		long[][] dp = new long[N+1][10];
+		
+		// 1자리 계단수의 경우의 수 각 초기화(1~9 각 1개씩)
+		for (int j=1; j<=9; j++) {
+			dp[1][j] = 1;
+		}
+		
+		// 2자리 계딴수부터 작업 시작
+		for (int i=2; i<=N; i++) {
+			for (int j=0; j<=9; j++) {
+				if (j==0) {
+					dp[i][j] = dp[i-1][1];
+					continue;
+				}
+				if (j==9) {
+					dp[i][j] = dp[i-1][8];
+					continue;
+				}
+				
+				dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1])%1000000000;
+			}
+		}
+		
+		long ans = 0;
+		for (long n : dp[N]) {
+			ans += n;
+		}
+		
+		System.out.println(ans%1000000000);
+	}
 }
