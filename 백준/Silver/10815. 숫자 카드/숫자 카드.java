2a8ff8bcd9febdda6a3c0a4b8614e5da8e2100ec
SeonggeunPark <162011004+SeonggeunPark@users.noreply.github.com>
2024-12-24 17:59:30 +0900

    [Silver V] Title: 숫자 카드, Time: 872 ms, Memory: 137756 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,41 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
-/*
- */
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
-		int N = Integer.parseInt(br.readLine());
-		boolean[] exist = new boolean[20000000];	// 해당 숫자 존재 여부 저장
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i=0; i<N; i++) {
-			exist[Integer.parseInt(st.nextToken())+10000000] = true;	//숫자 입력 받아 존재 체크
-		}
-		
-		int M = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine());
-		for (int i=0; i<M; i++) {
-			if (exist[Integer.parseInt(st.nextToken())+10000000]) {
-				sb.append('1').append(' ');
-			} else {
-				sb.append('0').append(' ');
-			}
-		}
-		
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int N = Integer.parseInt(br.readLine());
+        HashSet<Integer> set = new HashSet<>();
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            set.add(Integer.parseInt(st.nextToken())); // set에 숫자 추가
+        }
+        
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            sb.append(set.contains(num) ? '1' : '0').append(' '); // 존재 여부 확인 후 1 또는 0 추가
+        }
+        
+        System.out.println(sb.toString().trim()); // 마지막에 불필요한 공백 제거
+    }
 }
