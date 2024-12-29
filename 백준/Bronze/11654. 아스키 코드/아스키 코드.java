e46d45985a09ff545e01db7e0a9e7af911d3ac43
SeonggeunPark <162011004+SeonggeunPark@users.noreply.github.com>
2024-12-29 23:45:09 +0900

    [Bronze V] Title: 아스키 코드, Time: 104 ms, Memory: 14336 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+import org.w3c.dom.Node;
+
+public class Main {
+	public static void main(String[] args) throws IOException {	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		char[] arr = br.readLine().toCharArray();
+		
+		System.out.println((int)arr[0]);
+		
+	}
+}
