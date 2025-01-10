fe3ea7162286ce8b3cd186076657d893758ee4ae
SeonggeunPark <162011004+SeonggeunPark@users.noreply.github.com>
2025-01-10 20:08:35 +0900

    [Silver V] Title: 방 번호, Time: 68 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		char[] room = br.readLine().toCharArray();
+		int[] count = new int[10];
+		
+		// 숫자가 나온 횟수를 기록 
+		for (char c : room) {
+			int num = c-'0';
+			count[num] += 1;
+		}
+		
+		// 6과 9 횟수 평탄화
+		int add = count[6]+count[9];
+		if (add%2==0) {
+			count[6] = add/2;
+			count[9] = count[6];
+		} else {
+			count[6] = add/2;
+			count[9] = count[6]+1;
+		}
+		
+		// 최대값 찾기
+		int max = -1;
+		for (int i=0; i<=9; i++) {
+			if (count[i] > max) {
+				max = count[i];
+			}
+		}
+		
+		System.out.println(max);
+	}
+}
