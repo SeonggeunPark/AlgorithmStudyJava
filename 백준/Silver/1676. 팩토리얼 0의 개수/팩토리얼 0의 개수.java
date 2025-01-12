4026a157b41019eadf7fa5cdf9bbf05d459b848a
SeonggeunPark <162011004+SeonggeunPark@users.noreply.github.com>
2025-01-12 14:38:27 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 64 ms, Memory: 11512 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -9,6 +9,15 @@ import java.util.Scanner;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
+/*
+ * 숫자 끝에 0이 붙으려면 어떤 수에 10을 곱해야 함
+ * 팩토리얼 에서 10이 곱해지는 경우는 2*5뿐임
+ * 따라서 N!에서 0의 개수는 1부터 N까지 모든 수를 소인수분해했을 때
+ * 2,5 짝의 개수를 구하면 됨.
+ * 
+ * 여기서 2의 개수는 2, 4, 8 ... 로 5의 개수보다 확실히 많으므로,
+ * => 결국 5의 개수를 세면 된다.
+ */
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
