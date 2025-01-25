a3bb0bec90eb213913e6944640bfb2df576b33f8
SeonggeunPark <162011004+SeonggeunPark@users.noreply.github.com>
2025-01-25 22:22:47 +0900

    [Bronze IV] Title: 기찍 N, Time: 180 ms, Memory: 18080 KB -BaekjoonHub

--- 백준/Bronze/2742. 기찍 N/기찍 N.java
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -1,11 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
         for (int i=N; i>=1; i--) {
-            System.out.println(i);
+            sb.append(i).append('\n');
         }
+        
+        System.out.println(sb);
     }
 }
