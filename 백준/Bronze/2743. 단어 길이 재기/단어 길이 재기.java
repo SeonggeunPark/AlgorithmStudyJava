e8936a22ebf378f4652d743f78e6a3c1ce89dd25
SeonggeunPark <162011004+SeonggeunPark@users.noreply.github.com>
2025-02-17 23:26:50 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 164 ms, Memory: 17612 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception, IOException {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println(sc.next().length());
+	}
+}
