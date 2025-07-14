add9adf2d466367a19e2633057a3b8d27c35d281
박성근 <qkrtjd50@naver.com>
2025-07-14 19:48:55 +0900

    [Silver I] Title: 신입 사원, Time: 888 ms, Memory: 299588 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -5,41 +5,21 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	static class Candidate implements Comparable<Candidate>{
-		int doc;
-		int interview;
-		public Candidate() {		}
-		
-		public Candidate(int doc, int interview) {
-			this.doc = doc;
-			this.interview = interview;
-		}
-
-		@Override
-		public int compareTo(Main.Candidate o) {
-			return this.doc - o.doc;
-		}
-	}
 	static int T, N, ans;
-	static Candidate[] candidates;
+	static int[] docs; // 등수별로 지원자 번호 저장
+	static int[] interviews;	// 지원자 번호 별로 등수 저장
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	public static void main(String[] args) throws IOException {
 		T = Integer.parseInt(br.readLine());
 		for (int t=1; t<=T; t++) {
 			init();
-			// 서류등수로 오름차순 정렬
-			Arrays.sort(candidates);
 			
-			int min = 100001; // 최고등수 초기화
-			for (Candidate c : candidates) {
-//				int doc = c.doc;
-				int interview = c.interview;
-				
-				if (interview < min) {
+			int min = interviews[docs[1]]; // 최고등수 초기화
+			for (int i=2; i<=N; i++) {
+				if (interviews[docs[i]] < min) {
 					ans += 1;
+					min = Math.min(interviews[docs[i]], min);
 				}
-				
-				min = Math.min(interview, min);
 			}
 			
 			System.out.println(ans);
@@ -48,13 +28,17 @@ public class Main {
 	}
 
 	private static void init() throws IOException {
-		ans = 0;
+		ans = 1;
 		N = Integer.parseInt(br.readLine());
-		candidates = new Candidate[N];
-		
-		for (int i=0; i<N; i++) {
+		docs = new int[N+1];
+		interviews = new int[N+1];
+		for (int i=1; i<=N; i++) {
 			StringTokenizer st  = new StringTokenizer(br.readLine());
-			candidates[i] = new Candidate(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+			int doc = Integer.parseInt(st.nextToken());
+			int interview = Integer.parseInt(st.nextToken());
+			
+			docs[doc] = i;
+			interviews[i] = interview;
 		}
 	}
 }
