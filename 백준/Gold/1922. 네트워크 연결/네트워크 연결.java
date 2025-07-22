aa75ea69674f2260909709b9f8eaa1eebaa0d2ad
박성근 <qkrtjd50@naver.com>
2025-07-22 11:19:23 +0900

    [Gold IV] Title: 네트워크 연결, Time: 540 ms, Memory: 49028 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -8,7 +8,7 @@ import java.util.StringTokenizer;
 public class Main {
 	static StringBuilder sb;
 	static int N, M, ans, pick;
-	static int[] p;
+	static int[] p, r;
 	static Edge[] edges;
 	static boolean[] visited;
 
@@ -51,11 +51,12 @@ public class Main {
 				pick += 1;
 				ans += curr.w;
 			}
-			
-			// 선택한 간선 수가 (노드수-1) 이 되면 작업 종료 
-			if (pick >= N-1) break;
+
+			// 선택한 간선 수가 (노드수-1) 이 되면 작업 종료
+			if (pick >= N - 1)
+				break;
 		}
-		
+
 		System.out.println(ans);
 	}
 
@@ -78,19 +79,31 @@ public class Main {
 
 		// 상호배타 집합을 위한 배열
 		p = new int[N + 1];
+		r = new int[N + 1];
 		for (int i = 1; i <= N; i++) {
 			p[i] = i;
+			r[i] = 0;
 		}
 	}
 
 	static void union(int a, int b) {
-		p[find(b)] = find(a);
+		int pa = find(a);
+		int pb = find(b);
+		
+		if (r[pa] < r[pb]) {
+			p[pa] = pb;
+		} else if (r[pa] > r[pb]) {
+			p[pb] = pa;
+		} else {
+			p[pb] = pa;
+			r[pa] += 1;
+		}
 	}
 
 	static int find(int a) {
 		if (p[a] != a) {
 			return p[a] = find(p[a]);
 		}
-		return a;
+		return p[a];
 	}
 }
