128b418b08c01d38a66819640918bcc0187165b1
박성근 <qkrtjd50@naver.com>
2025-08-04 15:36:25 +0900

    [Bronze IV] Title: 특별한 날, Time: 172 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int M = sc.nextInt();
+        int D = sc.nextInt();
+
+        if (M < 2 || (M == 2 && D < 18)) {
+            System.out.println("Before");
+        } else if (M == 2 && D == 18) {
+            System.out.println("Special");
+        } else {
+            System.out.println("After");
+        }
+    }
+}
