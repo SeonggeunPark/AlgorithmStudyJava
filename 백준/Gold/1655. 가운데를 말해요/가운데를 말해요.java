85d6485f9c0d5fd5e12a93fba96c02b973d78f78
박성근 <qkrtjd50@naver.com>
2025-08-28 17:14:48 +0900

    [Gold II] Title: 가운데를 말해요, Time: 420 ms, Memory: 34236 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
@@ -1,62 +1,48 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collections;
 import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.StringTokenizer;
 
-/*
- *  숫자가 주어질 때마다 삽입정렬.
- *  삽입 위치 찾는건 이분탐색으로.
- *  중간값은 짝수->len/2-1 홀수->len/2 
- */
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringBuilder sb;
-	static int N;
-	static List<Integer> list;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb;
+    static int N;
 
-	public static void main(String[] args) throws IOException {
-		init();
-		
-		for (int i=0; i<N; i++) {
-			int newNum = Integer.parseInt(br.readLine());
-			int targetIdx = binarySearch(0, list.size()-1, newNum);
-			
-			list.add(targetIdx, newNum);
-			
-			int midIdx = list.size()/2;
-			if (list.size()%2 == 0) midIdx -= 1;
-			
-			sb.append(list.get(midIdx)).append('\n');
-		}
-		
-		System.out.println(sb);
-	}
+    static PriorityQueue<Integer> maxHeap;  // 최대힙 (작은 수들의 절반)
+    static PriorityQueue<Integer> minHeap; // 최소힙 (큰 수들의 절반)
 
-	private static int binarySearch(int s, int e, int target) {
-		int mid = (s+e)/2;
-		if (s > e) {
-			return s;
-		}
-		
-		int midNum = list.get(mid);
-		if (midNum == target) {
-			return mid;
-		} else if(midNum < target) {
-			return binarySearch(mid+1, e, target);
-		} else {
-			return binarySearch(s, mid-1, target);
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        init();
 
-	private static void init() throws IOException {
-		sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		list = new ArrayList<>();
-	}
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+            insert(x);
+            sb.append(maxHeap.peek()).append('\n'); // 항상 아래 중앙값 출력
+        }
+
+        System.out.print(sb);
+    }
+
+    private static void init() throws IOException {
+        sb = new StringBuilder();
+        N = Integer.parseInt(br.readLine().trim());
+        maxHeap  = new PriorityQueue<>(Collections.reverseOrder()); 
+        minHeap = new PriorityQueue<>();                           
+    }
+
+    private static void insert(int n) {
+        // maxheap을 우선으로 담기
+        if (maxHeap.isEmpty() || n <= maxHeap.peek()) maxHeap.offer(n);
+        else minHeap.offer(n);
+
+        if (maxHeap.size() < minHeap.size()) maxHeap.offer(minHeap.poll());
+        if (maxHeap.size() > minHeap.size() + 1) minHeap.offer(maxHeap.poll());
+
+        if (!minHeap.isEmpty() && maxHeap.peek() > minHeap.peek()) {
+            int a = maxHeap.poll(), b = minHeap.poll();
+            maxHeap.offer(b);
+            minHeap.offer(a);
+        }
+    }
 }
