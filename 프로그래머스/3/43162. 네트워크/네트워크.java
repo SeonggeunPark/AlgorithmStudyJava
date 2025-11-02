46193478c8ba78bd9ebbfe11dccf5d092e87e5c9
박성근 <qkrtjd50@naver.com>
2025-11-02 15:32:43 +0900

    [level 3] Title: 네트워크, Time: 0.22 ms, Memory: 89.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,25 +1,24 @@
 import java.util.*;
 
-
 class Solution {
     public int solution(int n, int[][] computers) {
+        boolean[] visited = new boolean[n];
+        
         int answer = 0;
-	
-	boolean[] visited = new boolean[n];
-	for (int i=0; i<n; i++) {
-		if (visited[i]) continue;
-		dfs(i, n, visited, computers);
-		answer+=1;
-	}
-
+        for (int i=0; i<n; i++) {
+            if (visited[i]) continue;
+            dfs(i, n, visited, computers);
+            answer += 1;
+        }
+        
         return answer;
     }
     public void dfs(int v, int n, boolean[] visited, int[][] computers) {
+        if (visited[v]) return;
+        visited[v] = true;
+        
         for (int i=0; i<n; i++) {
-            if (i==v) continue;
-            if (computers[v][i]==0) continue;
-            if (visited[i]) continue;
-            visited[i] = true;
+            if (computers[v][i] != 1) continue;
             dfs(i, n, visited, computers);
         }
     }
