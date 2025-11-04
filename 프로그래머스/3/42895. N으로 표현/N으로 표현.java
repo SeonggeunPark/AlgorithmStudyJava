360439e67794e90e255f136ef984d11ed3c6fc37
박성근 <qkrtjd50@naver.com>
2025-11-04 17:11:56 +0900

    [level 3] Title: N으로 표현, Time: 35.88 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int N, int number) {
+        int answer = 0;
+        Set<Integer>[] dp = new Set[9];
+        for (int i=1; i<9; i++) {
+            dp[i] = new HashSet<>();
+        }
+        for (int i=1; i<=5; i++) {
+            int num = makeNum(N,i);
+            dp[i].add(num);
+        }
+        
+        if (N==number) return 1;
+        
+        // 시작
+        for (int i=2; i<=8; i++) {
+            for (int j=1; j<i; j++) {
+                cal(i, j, i-j, dp, number);
+            }
+        }
+
+        for (int i=1; i<=8; i++) {
+            if (dp[i].contains(number)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    private int makeNum(int n, int cnt) {
+        int res = 0;
+        while (cnt>0) {
+            res += n;
+            n*=10;
+            cnt-=1;
+        }
+        return res;
+    }
+    private void cal (int target, int l, int r, Set<Integer>[] dp, int number) {
+        // 사칙연산
+        for (int lv : dp[l]) {
+            for (int rv: dp[r]) {
+                dp[target].add(lv+rv);
+                dp[target].add(lv-rv);
+                dp[target].add(lv*rv);
+                if (rv>0){
+                    dp[target].add(lv/rv);
+                }
+            }
+        }
+    }
+}
