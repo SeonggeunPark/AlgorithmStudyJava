a95fe18fbecc9c89a812f584cf859b744ba3d1cd
박성근 <qkrtjd50@naver.com>
2025-12-08 16:31:09 +0900

    [level 3] Title: 부대복귀, Time: 157.17 ms, Memory: 188 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.java
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.java
@@ -4,8 +4,6 @@ class Solution {
     public final int INF = Integer.MAX_VALUE;
     public int[] solution(int n, int[][] roads, int[] sources, int destination) {
         int[] answer = new int[sources.length];
-        int aIdx=0;
-        
         List<Integer>[] adjList = new List[n+1];
         for (int i=1; i<=n; i++) {
             adjList[i] = new ArrayList<>();
@@ -15,52 +13,39 @@ class Solution {
             adjList[road[1]].add(road[0]);
         }
         
+        // int aIdx = 0;
         // for (int source : sources) {
         //     answer[aIdx++] = dijkstra(source, destination, adjList, n);
         // }
-        Map<Integer, Integer> sourceMap = new HashMap<>();
-        BFS(destination, sources, n, sourceMap, adjList);
+        
+        int[] visited =new int[n+1];
+        Arrays.fill(visited, -1);
+        BFS(destination, sources, n, adjList, visited);
         for (int i=0; i<sources.length; i++) {
-            answer[i] = sourceMap.get(sources[i]);
+            answer[i] = visited[sources[i]];
         }
         
         return answer;
     }
     // 목표지점부터 sources까지 탐색
-    public void BFS(int s, int[] sources, int n, Map<Integer, Integer> sourceMap, List<Integer>[] adjList) {
+    public void BFS(int s, int[] sources, int n, List<Integer>[] adjList, int[] visited) {
         Queue<Integer> q = new ArrayDeque<>();
         q.offer(s);
-        boolean[] visited =new boolean[n+1];
-        visited[s] = true;
-        int find = 0;
+        visited[s] = 0;
         int size =1 ;
-        int cnt = 0;
+        int cnt = 1;
         
         while(!q.isEmpty()) {
-            if (find >= sources.length) return;
             for (int i=1; i<=size; i++) {
                 int poll = q.poll();
-                for (int source:sources) {
-                    if (poll==source) {
-                        sourceMap.put(poll, cnt);
-                        find+=1;
-                        break;
-                    }
-                }
                 for (int next : adjList[poll]) {
-                    if (visited[next]) continue;
+                    if (visited[next] > -1) continue;
                     q.offer(next);
-                    visited[next] = true;
+                    visited[next] = cnt;
                 }
             }
             size = q.size();
             cnt+=1;
-        }
-        
-        for (int source : sources) {
-            if (sourceMap.get(source) == null) {
-                sourceMap.put(source, -1);
-            }
         }
     }
     public class Node implements Comparable<Node> {
