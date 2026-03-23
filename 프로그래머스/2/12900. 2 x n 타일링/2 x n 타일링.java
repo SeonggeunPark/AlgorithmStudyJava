13c983537743bbde85d4d58c582f787f7631be51
박성근 <qkrtjd50@naver.com>
2026-03-23 14:41:51 +0900

    [level 2] Title: 2 x n 타일링, Time: 2.16 ms, Memory: 55 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i=3; i<=n; i++) {
+            dp[i] = (dp[i-2]+dp[i-1])%1000000007;
+        }
+        return dp[n];
+    }
+}
+/*
+n의 경우의 수
+n-1 + 1개
+n-2 + 가로타일 2개
+*/
