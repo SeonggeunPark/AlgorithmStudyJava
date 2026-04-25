83fd1d65e27ce3919406687d00ded68c9e3cf8a9
박성근 <qkrtjd50@naver.com>
2026-04-25 16:33:34 +0900

    [Gold V] Title: 탑, Time: 700 ms, Memory: 83096 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,45 +1,39 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
+
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		// 탑의 수
-		int N = Integer.parseInt(br.readLine());
-		// 탑 배치
-		int[] arr = new int[N];
-		// 정답 배열
-		int[] ans = new int[N];
-		// 가장 가까운 높은 탑의 인덱스를 저장해두기 위한 스택
-		Stack<Integer> stack = new Stack<>();
-		// 탑 높이 입력
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i=0; i<N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		// 탐색 시작
-		for (int i=0; i<N; i++) {
-			int currTowerHeight = arr[i];
-			// 나보다 높은 탑이 나올 때까지 스택 조회
-			while(!stack.isEmpty()) {
-				int popTowerIdx = stack.pop();
-				if (currTowerHeight >= arr[popTowerIdx]) continue;
-				// 나보다 큰 탑 나옴
-				ans[i] = popTowerIdx+1;
-				stack.push(popTowerIdx);
-				break;
-			}
-			
-			stack.push(i);
-		}
-		
-		for (int i=0; i<N; i++) {
-			System.out.print(ans[i] + " ");
-		}
+	public static void main(String[] args) throws Exception {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        int n = Integer.parseInt(br.readLine());
+	        int[] towers = new int[n];
+	        int[] answer = new int[n];
+	        StringTokenizer s = new StringTokenizer(br.readLine());
+	        for (int i=0; i<n; i++) {
+	            towers[i] = Integer.parseInt(s.nextToken());
+	        }
+
+	        // 인덱스 기록 스택
+	        Stack<Integer> st = new Stack<>();
+	        for (int i=0; i<n; i++) {
+	            // 스택과 비교
+	            // 높이가 같거나 높으면 기록 후 push
+	            // 낮으면 pop (같거나 높은거 나올때까지)
+	            while (!st.isEmpty() && towers[st.peek()]<towers[i]) {
+	                st.pop();
+	            }
+
+	            if (st.isEmpty()) {
+	                answer[i] = 0;
+	            } else {
+	                answer[i] = st.peek()+1;
+	            }
+	            st.push(i);
+	        }
+	        StringBuilder sb = new StringBuilder();
+	        for (int i=0; i<n; i++) {
+	            sb.append(answer[i]).append(' ');
+	        }
+	        System.out.println(sb.toString());
 	}
 }
