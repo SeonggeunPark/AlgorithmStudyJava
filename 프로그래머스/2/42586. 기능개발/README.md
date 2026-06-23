9bf01065ca6d16375bf250cc3b3ced4d1d1aa13c
박성근 <qkrtjd50@naver.com>
2026-06-23 22:06:47 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 77.8 MB, 시간: 0.04 ms
+메모리: 80.3 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 04일 12:37:33
+2026년 06월 23일 22:06:43
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,36 +2,32 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int[] remains = new int[progresses.length];
-        for (int i=0; i<progresses.length; i++ ){
-            int remain = 0;
-            if ((100-progresses[i])%speeds[i] == 0) {
-                remain = (100-progresses[i])/speeds[i];
-            } else {
-                remain = (100-progresses[i])/speeds[i] + 1;
+        List<Integer> res = new ArrayList<>();
+        int sum = 0;
+        int maxNeeds = 0;
+        for (int i=0; i<progresses.length; i++) {
+            int remain = 100-progresses[i];
+            int needs = remain%speeds[i]==0 ? remain/speeds[i] :remain/speeds[i]+1;
+            if (maxNeeds==0) {
+                sum=1;
+                maxNeeds=needs;
+                continue;
             }
-            remains[i]=remain;
-        }
-        List<Integer> tmpList = new ArrayList<Integer>();
-        
-        int time = 0;
-        int cnt = 1;
-        for (int i=0; i<remains.length; i++) {
-            if (time < remains[i]) {
-                if (i!=0) {
-                    tmpList.add(cnt);
-                }
-                time = remains[i];
-                cnt = 1;
+            if (maxNeeds >= needs) {
+                sum+=1;
             } else {
-                cnt += 1;
+                res.add(sum);
+                sum=1;
+                maxNeeds=needs;
             }
         }
-        tmpList.add(cnt);
+        if (sum > 0) {
+            res.add(sum);
+        }
         
-        int[] answer = new int[tmpList.size()];
-        for (int i=0; i<tmpList.size(); i++) {
-            answer[i] = tmpList.get(i);
+        int[] answer = new int[res.size()];
+        for (int i=0; i<res.size(); i++) {
+            answer[i] = res.get(i);
         }
         return answer;
     }
