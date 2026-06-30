60fe5eeeaf484273937755a14f4cb66f27f1d657
박성근 <qkrtjd50@naver.com>
2026-06-30 22:01:32 +0900

    [level 2] Title: 더 맵게, Time: 676.51 ms, Memory: 140 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,26 +1,10 @@
 import java.util.*;
-/*
-우선순위 큐,
-맨 앞 값이 K보다 높을 때까지 연산.
-*/
 
 class Solution {
     public int solution(int[] scoville, int K) {
-        int answer = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
-        for (int s : scoville) {
-            pq.offer(s);
-        }
-        
-        while (pq.size()>1) {
-            int s1 = pq.poll();
-            if (s1>=K) break;
-            int s2 = pq.poll();
-            
-            pq.offer(s1+s2*2);
-            
-            answer+=1;
-        }
-        return answer = pq.peek()>=K ? answer : -1;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int s : scoville)  pq.offer(s);
+        while (pq.size() > 1 && pq.peek()<K)    pq.offer(pq.poll()+pq.poll()*2);
+        return pq.peek()>=K ? scoville.length-pq.size() : -1;
     }
 }
