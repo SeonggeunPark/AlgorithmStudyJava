431c68e741c5399462d7833bfc9f170b53e56bac
박성근 <qkrtjd50@naver.com>
2026-07-05 23:03:40 +0900

    [level 2] Title: 가장 큰 수, Time: 95.22 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
 
 ### 성능 요약
 
-메모리: 393 MB, 시간: 2937.17 ms
+메모리: 127 MB, 시간: 95.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 04일 15:08:58
+2026년 07월 05일 23:03:36
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,31 +1,25 @@
 import java.util.*;
+
 class Solution {
-    class Number implements Comparable<Number> {
-        int idx;
-        String val;
-        Number(int idx, String val) {
-            this.idx = idx;
-            this.val = val+val+val;
-        }
-        @Override
-        public int compareTo(Number o) {
-            return o.val.compareTo(this.val);
-        }
-    }
     public String solution(int[] numbers) {
-        Number[] sorted = new Number[numbers.length];
-        for (int i=0; i<numbers.length; i++) {
-            sorted[i] = new Number(i, String.valueOf(numbers[i]));
+        String[] arr = new String[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            arr[i] = String.valueOf(numbers[i]);
         }
-        
-        Arrays.sort(sorted);
-        
-        String answer = "";
-        for (int i=0; i<numbers.length; i++) {
-            answer += numbers[sorted[i].idx];
+
+        Arrays.sort(arr, (a, b) -> (b + a).compareTo(a + b));
+
+        if (arr[0].equals("0")) {
+            return "0";
         }
-        
-        if (answer.charAt(0)=='0') answer = "0";
-        return answer;
+
+        StringBuilder sb = new StringBuilder();
+
+        for (String num : arr) {
+            sb.append(num);
+        }
+
+        return sb.toString();
     }
 }
