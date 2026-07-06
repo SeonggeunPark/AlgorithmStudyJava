71ea1ae201c3cde5adb90ac0cfff6e3963e4f5ed
박성근 <qkrtjd50@naver.com>
2026-07-06 23:47:17 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 2.70 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,90 +1,64 @@
 import java.util.*;
 
-/*
-1. 대기큐에 뭐가 있으면 꺼내서 작업 수행
-    - 소요시간 짧은순, 요청시간 빠른 순, 번호 작은 순
-2. 작업 중간에 변경 X
-3. 문맥교환 시간 소요 X
-    - 작업 종료 - 새 작업 큐 삽입 - 큐에서 꺼내 새 작업 수행
-
-*/
 class Solution {
-    public class Task implements Comparable<Task> {
-        int num; // 번호
-        int loadTime; // 소요시간
-        int reqTime; // 요청시간
-        int startTime; // 실제 작업 시작시간
-        public Task (int num, int loadTime, int reqTime) {
-            this.num = num;
-            this.loadTime = loadTime;
-            this.reqTime = reqTime;
-            // this.startTime = startTime;
+    public class Job implements Comparable<Job> {
+        int n, rt, tt;
+        public Job(int n, int rt, int tt) {
+            this.n = n;
+            this.rt = rt;
+            this.tt= tt;
         }
         @Override
-        public int compareTo (Task o) {
-            if (this.loadTime==o.loadTime) {
-                if (this.reqTime==o.reqTime) {
-                    return this.num-o.num;
+        public int compareTo(Job o) {
+            if (o.tt == this.tt) {
+                if (o.rt == this.rt) {
+                    return this.n - o.n;
                 }
-                return this.reqTime-o.reqTime;
+                return this.rt - o.rt;
             }
-            return this.loadTime-o.loadTime;
+            return this.tt - o.tt;
         }
-    }
+    } 
     public int solution(int[][] jobs) {
-        // 우선순위 대기큐 생성
-        PriorityQueue<Task> pq = new PriorityQueue<>();
-        
-        int[] result = new int[jobs.length];
-        int rIdx = 0;
-        int jIdx = 0;
-        int time= 0;
-        // 요청시각마다 요청 큐에 삽입
-        // 종료시간마다 종료 태스크 기록(종료시각 - 요청시각)
-        Arrays.sort(jobs, (o1, o2) -> {
-           return o1[0] - o2[0]; 
+        int answer = 0;
+        int len = jobs.length;
+        // 1. 시작순서대로 job 정렬
+        Job[] jobArr = new Job[len];
+        for (int i=0; i<len; i++) {
+            jobArr[i] = new Job(i, jobs[i][0], jobs[i][1]);
+        }
+        Arrays.sort(jobArr, new Comparator<Job>() {
+            @Override
+            public int compare(Job o1, Job o2) {
+                return o1.rt - o2.rt;
+            }
         });
-        
-        // 체크 타이밍: 
-        // 1. 진행중인 작업 끝날때
-        // 2. 새 작업 추가해야할 때 (job[0])
-        // 종료 타이밍: 추가할 작업이 없고, 진행중인 작업도 없을 때
-        Task cur = null;
-        while (cur!=null || jIdx < jobs.length) {
-            // 다음 time으로 넘어가기 => 현재작업이 종료되는 시점 vs 다음 작업 삽입되는 시점 짧은것
-            if (cur==null) {
-                time = jobs[jIdx][0];
-            } else if(jIdx >= jobs.length) {
-                time = cur.startTime+cur.loadTime;
-            } else {
-                time = Math.min(jobs[jIdx][0], cur.startTime+cur.loadTime);
-            }
-            // System.out.println("현재시간 -> "+time );
-            
-            // 진행중 작업 체크
-            if (cur!=null && cur.startTime+cur.loadTime == time) {
-                result[rIdx++] = time-cur.reqTime;
-                cur = null; // 작업 해제
+
+        // 2. job 순차처리
+        PriorityQueue<Job> pq = new PriorityQueue<>();
+        int idx = 0;
+        int time = 0;
+        int cnt = 0;
+        // 이벤트단위 점프: (1) 현재 작업 종료, (2) 다음 작업 삽입
+        while (cnt < len) {
+            // 대기큐 삽입
+            while (idx<len && jobArr[idx].rt<=time) {
+                pq.offer(jobArr[idx++]);
             }
             
-            // 새 작업 큐 삽입
-            while (jIdx<jobs.length && jobs[jIdx][0] == time) {
-                pq.offer(new Task(jIdx, jobs[jIdx][1], jobs[jIdx][0]));
-                jIdx+=1;
+            // 대기큐에 작업 없으면 다음 작업 시작시간까지 시간 경과
+            if (pq.isEmpty()) {
+                time = jobArr[idx].rt;
+                continue;
             }
             
-            // 작업중인 것이 없고, 대기큐에 뭔가 있으면 작업 시작
-            if (cur==null && !pq.isEmpty()) {
-                cur = pq.poll();
-                cur.startTime=time;
-            }
-        }
-        int sum = 0;
-        for (int i=0; i<result.length; i++) {
-            sum += result[i];
+            // 작업 시작
+            Job curJob = pq.poll();
+            answer += (time+curJob.tt)-curJob.rt;
+            cnt+=1;
+            time += curJob.tt;
         }
         
-        int answer = sum/result.length;
-        return answer;
+        return answer/len;
     }
 }
