9aa9d70a0cad5b87c9b9f0e890440c27a5010a94
박성근 <qkrtjd50@naver.com>
2026-07-08 00:41:47 +0900

    [level 2] Title: 가장 큰 수, Time: 80.69 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -8,7 +8,12 @@ class Solution {
             arr[i] = String.valueOf(numbers[i]);
         }
 
-        Arrays.sort(arr, (a, b) -> (b + a).compareTo(a + b));
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                return (s2+s1).compareTo(s1+s2);
+            }
+        });
 
         if (arr[0].equals("0")) {
             return "0";
