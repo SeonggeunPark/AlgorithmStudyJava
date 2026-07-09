9a7aa05669cd2f77560a0d409abf7f9f657edac7
박성근 <qkrtjd50@naver.com>
2026-07-09 22:46:08 +0900

    [level 2] Title: 소수 찾기, Time: 6.09 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,56 +1,51 @@
 import java.util.*;
 
 class Solution {
-    int len, answer;
-    Map<Integer,Boolean> check;
+    Set<Integer> set = new HashSet<>();
+
     public int solution(String numbers) {
-        len = numbers.length();
-        check = new HashMap<>();
-        answer = 0;
-        
-        // 완전탐색
-        bruteForce(0, 0, new StringBuilder(), numbers);
-        
+        int len = numbers.length();
+        StringBuilder sb = new StringBuilder();
+        boolean[] visited = new boolean[len];
+
+        countPrime(0, sb, numbers, visited, len);
+
+        int answer = 0;
+
+        for (int num : set) {
+            if (checkPrime(num)) answer++;
+        }
+
         return answer;
     }
-    public boolean checkPrime(int num) {
-        if(num<=1) return false;
-        for (int i=2; i<= (int) Math.sqrt(num); i++) {
-            if (num % i == 0) return false;
-        }
-        return true;
-    }
-    public void bruteForce(int idx, int visit, StringBuilder sb, String numbers) {
-        // 탐색 종료 시 소수 여부, 기탐색 여부 체크
-        if (idx >= len) {
-            if (sb.length()<=0) return;
-            int num = Integer.parseInt(sb.toString());
-            if (check.get(num)!=null) return;
-            
-            check.put(num, true);
-            
-            // 소수 체크
-            if (checkPrime(num)) {
-                answer++;
-            }
+
+    public void countPrime(int cnt, StringBuilder sb, String numbers, boolean[] visited, int len) {
+        if (cnt >= len) {
             return;
         }
-        
-        // 탐색
-        for (int i=0; i<len; i++) {
-            // 방문여부 체크
-            if ((visit & (1<<i)) == (1<<i)) continue;
-            char cur = numbers.charAt(i);
-            
-            int originLen = sb.length();
-            
-            // 채택안하는 경우
-            bruteForce(idx+1, visit, sb, numbers);
-            
-            // 채택하는 경우
-            bruteForce(idx+1, visit|(1<<i), sb.append(cur), numbers);
-            
-            sb.setLength(originLen);
+
+        for (int i = 0; i < len; i++) {
+            if (visited[i]) continue;
+
+            sb.append(numbers.charAt(i));
+            visited[i] = true;
+
+            set.add(Integer.parseInt(sb.toString()));
+
+            countPrime(cnt + 1, sb, numbers, visited, len);
+
+            sb.setLength(sb.length() - 1);
+            visited[i] = false;
         }
+    }
+
+    public boolean checkPrime(int num) {
+        if (num <= 1) return false;
+
+        for (int i = 2; i <= (int) Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+
+        return true;
     }
 }
