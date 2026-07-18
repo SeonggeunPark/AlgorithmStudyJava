31874d6ccbf8eb87e7eb24b7cf15c606e02a1c4e
박성근 <qkrtjd50@naver.com>
2026-07-18 15:57:19 +0900

    [level 2] Title: 카펫, Time: 4.06 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,22 +1,14 @@
-/*
-    3가지 조건 이용
-    1) 둘레 길이 = (b가로+b세로)*2 = brown + 4
-    2) yellow = yellow 넓이 = y가로 * y세로 = (b가로-2) * (b세로-2)
-    3) b가로 >= b세로
-*/
 class Solution {
     public int[] solution(int brown, int yellow) {
-        // b가로 + b세로 (갈색 타일은 항상 짝수임)
-        int bSum = (brown+4) / 2;
+        int sum = (brown+4)/2;
+        int prod = brown+yellow;
         
-        int height = bSum / 2;
-        int width = bSum - height;
-        
-        while (height > 0 && yellow != (width-2) * (height-2)) {
-            width += 1;
-            height -= 1;
+        for (int w=prod; w>=0; w--) {
+            if (prod%w!=0) continue;
+            int h = prod/w;
+            if (w+h==sum) return new int[]{w, h};
         }
-        
-        return new int[]{width, height};
+            
+        return null;        
     }
 }
