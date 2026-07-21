ec69f1ce294e1ea243cf779aaa8035ee04ad8c57
박성근 <qkrtjd50@naver.com>
2026-07-21 23:09:54 +0900

    [level 2] Title: 피로도, Time: 1.79 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,28 +1,20 @@
 import java.util.*;
 
 class Solution {
-    int answer = 0;
-    boolean[] visited;
-
     public int solution(int k, int[][] dungeons) {
-        visited = new boolean[dungeons.length];
-        
-        dfs(k, dungeons, 0);
-        
-        return answer;
+        return dfs(k, 0, 0, dungeons);
     }
-
-    public void dfs(int k, int[][] dungeons, int count) {
-        answer = Math.max(answer, count);
-
-        for (int i = 0; i < dungeons.length; i++) {
-            if (!visited[i] && k >= dungeons[i][0]) {
-                visited[i] = true;
-                
-                dfs(k - dungeons[i][1], dungeons, count + 1);
-                
-                visited[i] = false; 
-            }
+    public int dfs(int k, int cnt, int visit, int[][] dungeons) {
+        if (cnt >= dungeons.length) return cnt;
+        if (k <= 0) return cnt;
+        
+        int res = 0;
+        for (int i=0; i<dungeons.length; i++) {
+            if (((1<<i) & visit) == (1<<i)) continue;
+            if (k<dungeons[i][0]) continue;
+            
+            res = Math.max(res, dfs(k-dungeons[i][1], cnt+1, (visit|(1<<i)), dungeons));
         }
+        return Math.max(res, cnt);
     }
 }
