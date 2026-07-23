ccc13f27251377fe396df0e03e48638258676173
박성근 <qkrtjd50@naver.com>
2026-07-23 23:22:06 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.24 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,36 +1,35 @@
 import java.util.*;
-
+// 서브트리 개수 계산, 차이의 최소값 저장
+// 1이 루트노드라고 임의 지정
 class Solution {
     List<Integer>[] adjList;
     boolean[] visited;
     int answer;
     public int solution(int n, int[][] wires) {
         adjList = new List[n+1];
-        for (int i=1;i<=n; i++) {
+        for (int i=1; i<=n; i++) {
             adjList[i] = new ArrayList<>();
         }
-        
-        for (int[] wire : wires) {
+        for (int[] wire: wires ) {
             adjList[wire[0]].add(wire[1]);
             adjList[wire[1]].add(wire[0]);
         }
-        answer = n-1;
+        
+        answer = 100;
         visited = new boolean[n+1];
-        visited[1] = true;
-        DFS(1, n);
+        
+        countChilds(1, n);
+        
         return answer;
     }
-    public int DFS(int cur, int n) {
-        int res = 1;
-        
-        for (int next : adjList[cur]) {
-            if (visited[next]) continue;
-            visited[next] = true;
-            int subCnt = DFS(next, n);
-            answer = Math.min (answer, Math.abs(n - subCnt - subCnt));
-            res += subCnt;
+    public int countChilds(int root, int n) {
+        visited[root] = true;
+        int sum = 1;
+        for (int child : adjList[root]) {
+            if (visited[child]) continue;
+            sum += countChilds(child, n);
         }
-        
-        return res;
+        answer = Math.min(answer, Math.abs(sum-(n-sum)));
+        return sum;
     }
 }
